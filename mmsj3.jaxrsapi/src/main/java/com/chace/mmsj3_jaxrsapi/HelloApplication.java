649175fba26aa27d5b80df816e8385309a0a5341
Boris TCHASSEM NOUMENI <b.tchassem@sahelys.in>
2023-07-20 12:33:06 +0100

    added modules

---
+++ mmsj3.jaxrsapi/src/main/java/com/chace/mmsj3_jaxrsapi/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.chace.mmsj3_jaxrsapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
